78f512bcb936365d7adf84ac327abd8a2d4763fe
JanzCS <jacobjanzcs@gmail.com>
2021-04-18 14:20:53 -0500

    idk

--- script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-tmpfile=$(mktemp)
-./a.out $1 > $tmpfile
-if [ "$2" = 'pdf' ];
-then
-    pandoc $tmpfile -o output.pdf
-else
-    pandoc -f markdown -t docx $tmpfile -o output.docx
-fi
